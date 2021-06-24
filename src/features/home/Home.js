8c2be9bc6275eb480193dab241da659cfa75a343
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-24 13:45:38 +0200

    Worked on Home. working selectedStudentData. assignmentsList may be temporary, still figuring out how to add stuff to array in object in array.

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -3,11 +3,26 @@ import { useSelector } from "react-redux";
 
 const Home = () => {
   const studentData = useSelector((state) => state.studentData);
-  console.log("* studentData", studentData);
+  //   console.log("* studentData", studentData);
+
   const selectedStudentData = studentData.filter(
     (data) => data.isSelected === true
   );
-  console.log("* selectedStudentData", selectedStudentData);
+  //   console.log("* selectedStudentData", selectedStudentData);
+
+  let assignmentsList = [];
+  selectedStudentData.forEach((element) => {
+    console.log(element);
+    if (!assignmentsList.includes(element.assignment)) {
+      assignmentsList.push(element.assignment);
+    }
+  });
+  console.log("* assignmentsList", assignmentsList);
+
+  //   let assignmentRatings = [];
+  //   selectedStudentData.forEach((element) => {
+  //       if assignmentRatings.some()
+  //   })
 
   return (
     <main>
