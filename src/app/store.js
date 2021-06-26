b3aa21a9f99a19e6f0a5fd58fcdbd4e8fe681539
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-26 17:51:28 +0200

    Fixed an oops in store.

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 // import counterReducer from "../features/counter/counterSlice";
 import studentDataReducer from "../features/studentData/studentDataSlice";
 import studentsReducer from "../features/students/studentsSlice";
-import assignmentsSlice from "../features/assignments/assignmentsSlice";
+import assignmentsReducer from "../features/assignments/assignmentsSlice";
 
 export const store = configureStore({
   reducer: {
     // counter: counterReducer,
     studentData: studentDataReducer,
     students: studentsReducer,
-    assignments: assignmentsSlice,
+    assignments: assignmentsReducer,
   },
 });
