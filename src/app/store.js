8837cd5555416b7ad8bedabaebc3f56dd8391515
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-26 15:45:26 +0200

    studentSlice -> studentsSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import studentDataReducer from "../features/studentData/studentDataSlice";
-import studentsReducer from "../features/Students/studentSlice";
+import studentsReducer from "../features/Students/studentsSlice";
 
 export const store = configureStore({
   reducer: {
