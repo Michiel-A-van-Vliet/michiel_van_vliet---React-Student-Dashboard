0db4fca96efa4d766f6b3a97d75532e405e846ac
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-26 17:52:47 +0200

    removed counter example.

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import counterReducer from "../features/counter/counterSlice";
 import studentDataReducer from "../features/studentData/studentDataSlice";
 import studentsReducer from "../features/students/studentsSlice";
 import assignmentsReducer from "../features/assignments/assignmentsSlice";
 
 export const store = configureStore({
   reducer: {
-    // counter: counterReducer,
     studentData: studentDataReducer,
     students: studentsReducer,
     assignments: assignmentsReducer,
