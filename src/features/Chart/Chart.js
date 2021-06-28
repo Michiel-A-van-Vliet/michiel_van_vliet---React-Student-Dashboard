baec8596f4d328e9d020b694ab2272de26358531
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-28 20:15:45 +0200

    Worked on Chart. It now show a chart of half the averages. Todo: add other half of averages. Todo: make it look better, somehow...

--- src/features/Chart/Chart.js
+++ src/features/Chart/Chart.js
@@ -2,47 +2,61 @@ import React from "react";
 import { VictoryBar, VictoryChart, VictoryTheme } from "victory";
 import { useSelector } from "react-redux";
 
-const testData = [
-  { assignment: "w1d1", averageRating: 1 },
-  { assignment: "w1d2", averageRating: 4 },
-  { assignment: "w1d3", averageRating: 3 },
-  { assignment: "w1d4", averageRating: 2 },
-  { assignment: "w1d5", averageRating: 5 },
-  { assignment: "w2d1", averageRating: 1.3 },
-  { assignment: "w2d2", averageRating: 4.5 },
-  { assignment: "w2d3", averageRating: 3.5 },
-  { assignment: "w2d4", averageRating: 2.5 },
-  { assignment: "w2d5", averageRating: 1.5 },
-];
-
 const Chart = () => {
   const students = useSelector((state) => state.students);
   const assignments = useSelector((state) => state.assignments);
 
-  // functie die checkt of student is geselecteerd. input naam van student. return boolean isSelected.
   const isSelected = (studentName) => {
     let student = students.find((student) => student.name === studentName);
     return student.isSelected;
   };
 
-  // todo: functie die langs alle cursussen gaat.
-  //       als student is geselecteerd, dan meenemen in gemiddelde.
+  let ratingsList = [];
 
-  let funRatingsList = [];
-  let dificultyRatingsList = [];
   assignments.forEach((assignment) => {
-    console.log("assignment:", assignment);
-    console.log("assignment.funRatings:", assignment.funRatings);
-    console.log("assignment.difficultyRatings:", assignment.difficultyRatings);
+    const funRatings = assignment.funRatings;
+    const difficultyRatings = assignment.difficultyRatings;
+
+    const getAverage = (list) => {
+      if (list.length === 0) {
+        return 0;
+      } else {
+        let total = 0;
+        list.forEach((item) => {
+          total += item;
+        });
+        return total / list.length;
+      }
+    };
+
+    let funList = [];
+    funRatings.forEach((funRating) => {
+      if (isSelected(funRating.name)) {
+        funList.push(funRating.rating);
+      }
+    });
+
+    let difficultyList = [];
+    difficultyRatings.forEach((difficultyRating) => {
+      if (isSelected(difficultyRating.name)) {
+        difficultyList.push(difficultyRating.rating);
+      }
+    });
+
+    const newRating = {
+      assignment: assignment.name,
+      averageFunRating: getAverage(funList),
+      averageDifficultyRating: getAverage(difficultyList),
+    };
+
+    ratingsList.push(newRating);
   });
 
-  console.log("students:", students);
-  console.log("assignments:", assignments);
-  console.log("isSelected('Evelyn'):", isSelected("Evelyn"));
+  console.log(ratingsList);
 
   return (
     <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
-      <VictoryBar data={testData} x="assignment" y="averageRating" />
+      <VictoryBar data={ratingsList} x="assignment" y="averageFunRating" />
     </VictoryChart>
   );
 };
