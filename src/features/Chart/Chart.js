0ab296195c017af0ea99e91f58c2d08c47b42276
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-28 18:21:47 +0200

    Gewerkt aan Chart, functie isSelected.

--- src/features/Chart/Chart.js
+++ src/features/Chart/Chart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { VictoryBar, VictoryChart, VictoryTheme } from "victory";
+import { useSelector } from "react-redux";
 
 const testData = [
   { assignment: "w1d1", averageRating: 1 },
@@ -16,7 +17,32 @@ const testData = [
 
 // const testAssignmentsList = ["w1d1", "w1d2", "w1d3", "w1d4"];
 
+// var studentsList = [];
+// rawStudentData.forEach((element) => {
+//   if (!studentsList.includes(element.name)) {
+//     studentsList.push(element.name);
+//   }
+// });
+
+// todo: functie die langs alle cursussen gaat.
+//       als student is geselecteerd, dan meenemen in gemiddelde.
+
+// todo:
+
 const Chart = () => {
+  const students = useSelector((state) => state.students);
+  const assignments = useSelector((state) => state.assignments);
+
+  // functie die checkt of student is geselecteerd. input naam van student.  boolean isSelected.
+  const isSelected = (studentName) => {
+    let student = students.find((student) => student.name === studentName);
+    return student.isSelected;
+  };
+
+  console.log("students:", students);
+  console.log("assignments:", assignments);
+  console.log("isSelected('Evelyn'):", isSelected("Evelyn"));
+
   return (
     <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
       <VictoryBar data={testData} x="assignment" y="averageRating" />
