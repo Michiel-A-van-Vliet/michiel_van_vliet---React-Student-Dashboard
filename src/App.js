fd09edba79f59ead9588f618f40f93a52a887d5c
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-28 16:46:23 +0200

    Installed VictoryChart. Worked on Chart. It now show a chart of bogus data. Todo: get it to show real data.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 // import CounterApp from "./features/counter/CounterApp";
 // import StudentData from "./features/studentData/StudentData";
 import Home from "./features/home/Home";
+// import Chart from "./features/chart/Chart";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       {/* <StudentData /> */}
       <Home />
+      {/* <Chart /> */}
     </div>
   );
 }
--- src/features/Chart/Chart.js
+++ src/features/Chart/Chart.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory";
+
+const testData = [
+  { assignment: "w1d1", averageRating: 1 },
+  { assignment: "w1d2", averageRating: 4 },
+  { assignment: "w1d3", averageRating: 3 },
+  { assignment: "w1d4", averageRating: 2 },
+  { assignment: "w1d5", averageRating: 5 },
+  { assignment: "w2d1", averageRating: 1.3 },
+  { assignment: "w2d2", averageRating: 4.5 },
+  { assignment: "w2d3", averageRating: 3.5 },
+  { assignment: "w2d4", averageRating: 2.5 },
+  { assignment: "w2d5", averageRating: 1.5 },
+];
+
+// const testAssignmentsList = ["w1d1", "w1d2", "w1d3", "w1d4"];
 
 const Chart = () => {
-  return <p>Grafiekje</p>;
+  return (
+    <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
+      <VictoryBar data={testData} x="assignment" y="averageRating" />
+    </VictoryChart>
+  );
 };
 
 export default Chart;
