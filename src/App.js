4ea4978f4c465836bf0d764f4b5b5cad20596d3a
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-30 20:49:19 +0200

    Added routing to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import CounterApp from "./features/counter/CounterApp";
-// import StudentData from "./features/studentData/StudentData";
+import StudentData from "./features/studentData/StudentData";
 import Home from "./features/home/Home";
 // import Chart from "./features/chart/Chart";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <StudentData /> */}
-      <Home />
-      {/* <Chart /> */}
-    </div>
+    // <div className="App">
+    //   <Home />
+    //   <StudentData />
+    //   {/* <Chart /> */}
+    // </div>
+
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/data">Brongegevens</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+
+        <Switch>
+          <Route path="/data">
+            <StudentData />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
