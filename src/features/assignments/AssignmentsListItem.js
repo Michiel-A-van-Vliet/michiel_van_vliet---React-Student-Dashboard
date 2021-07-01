c041bef6303fdc49bdb3f09cb49869d4a62e3a74
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-07-01 13:14:21 +0200

    AssignmentsListItem

--- src/features/assignments/AssignmentsListItem.js
+++ src/features/assignments/AssignmentsListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const AssignmentsListItem = () => {
-  return <li>Een regel met info.</li>;
+const AssignmentsListItem = ({ assignment }) => {
+  return <li>{assignment.name}</li>;
 };
 
 export default AssignmentsListItem;
